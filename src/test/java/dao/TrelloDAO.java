23d85e40231949b175fac7adee2cababab61ca61
sproesser <matheus_sproesser@hotmail.com>
2014-09-14 16:57:06 -0300

    primeiro commit com template dos primeiros testes

---
+++ src/test/java/dao/TrelloDAO.java
@@ -0,0 +1,33 @@
+package dao;
+
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+public class TrelloDAO {
+
+	@Test
+	public void deveObterJSONdoQuadroTrello(){
+		assertTrue(true);
+	}
+	@Test
+	public void deveLerArquivoDeConfiguracao(){
+		assertTrue(false);
+	}
+	@Test
+	public void deveOrganizarInformacoesDoArquivoNoObjetoDeAcesso(){
+		
+	}
+	@Test
+	public void deveExtrairDoJSONAsInformacoesDoQuadro(){
+		
+	}
+	@Test
+	public void deveExtrairDoJSONAsInformacoesDasListas(){
+		
+	}
+	@Test
+	public void deveExtrairDoJSONAsInformacoesDosCards(){
+		
+	}
+}
